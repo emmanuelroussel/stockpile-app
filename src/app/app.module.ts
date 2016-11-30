d3e62d64b6e8c54710255d95e1d65dba0309240f
Emmanuel Roussel <emman.roussel@gmail.com>
2016-11-29 19:07:13 -0500

    Rename tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { InventoryPage } from '../pages/inventory/inventory';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    InventoryPage,
     HomePage,
     TabsPage
   ],
@@ -20,8 +18,7 @@ import { TabsPage } from '../pages/tabs/tabs';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    InventoryPage,
     HomePage,
     TabsPage
   ],
