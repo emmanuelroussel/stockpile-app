326957b42a31553ee1cb5a0f5904fa60d033f2f1
Adam Vigneaux <adamvigneaux@gmail.com>
2017-03-08 17:00:14 -0500

    Remove default Ionic comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,8 +37,6 @@ export class MyApp {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
