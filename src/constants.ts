52261badc9a7431a6e26c19473618b615ca05082
Emmanuel Roussel <emman.roussel@gmail.com>
2017-03-18 14:51:53 -0400

    Remove '+' from messages

--- src/constants.ts
+++ src/constants.ts
@@ -21,10 +21,10 @@ export class Messages {
   private static baseSingular = 'Item successfully ';
   private static basePlural = 'Item(s) successfully ';
   public static itemAdded = `${Messages.baseSingular} added!`;
-  public static itemEdited = `${Messages.baseSingular} + edited!`;
-  public static itemDeleted = `${Messages.baseSingular} + deleted!`;
-  public static itemsReturned = `${Messages.basePlural} + returned!`;
-  public static itemsRented = `${Messages.basePlural} + rented!`;
+  public static itemEdited = `${Messages.baseSingular} edited!`;
+  public static itemDeleted = `${Messages.baseSingular} deleted!`;
+  public static itemsReturned = `${Messages.basePlural} returned!`;
+  public static itemsRented = `${Messages.basePlural} rented!`;
   public static itemAlreadyRented = 'Item is already rented!';
   public static itemNotRented = 'Item is not currently rented!';
   public static itemAlreadyAdded = 'Item is already added to the list!';
