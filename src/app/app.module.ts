a1a24c82e5eaa250cdca6e1b2233b7011fa4b24b
Emmanuel Roussel <emman.roussel@gmail.com>
2017-03-22 22:17:20 -0400

    Change error handler to extends Ionic's

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
 import { Http } from '@angular/http';
 import { Storage } from '@ionic/storage';
@@ -39,9 +39,10 @@ Raven
   .config('https://a64af160cb8746709c9743f23f666b9b@sentry.io/150943')
   .install();
 
-export class RavenErrorHandler implements ErrorHandler {
+export class RavenErrorHandler extends IonicErrorHandler {
   handleError(err: any): void {
-    Raven.captureException(err.originalError);
+    super.handleError(err);
+    Raven.captureException(err.originalError || err);
   }
 }
 
