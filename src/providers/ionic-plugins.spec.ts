433e5b55c0c826b1e66cd68744fc0dbe06a02fd0
Emmanuel Roussel <emman.roussel@gmail.com>
2017-03-24 15:42:36 -0400

    Add missing unit test

--- src/providers/ionic-plugins.spec.ts
+++ src/providers/ionic-plugins.spec.ts
@@ -1,4 +1,4 @@
-import { PlatformMock, PlatformMockIsCore, BarcodeScannerMock, ToastMock } from '../mocks';
+import { PlatformMock, PlatformMockIsCore, PlatformMockIsCordova, ToastMock } from '../mocks';
 
 import { IonicPlugins } from './ionic-plugins';
 import { TestData } from '../test-data';
@@ -8,15 +8,22 @@ let ionicPlugins: IonicPlugins = null;
 describe('IonicPlugins Provider', () => {
 
   beforeEach(() => {
-    ionicPlugins = new IonicPlugins((<any> new PlatformMock), (<any> new BarcodeScannerMock), (<any> new ToastMock));
+    ionicPlugins = new IonicPlugins((<any> new PlatformMock), (<any> new ToastMock));
   });
 
   it('is created', () => {
     expect(ionicPlugins).toBeTruthy();
   });
 
+  it('shows toast if cordova is available', () => {
+    ionicPlugins = new IonicPlugins((<any> new PlatformMockIsCordova), (<any> new ToastMock));
+    spyOn(ionicPlugins.toast, 'showWithOptions').and.callThrough();
+    ionicPlugins.showToast(TestData.response.message);
+    expect(ionicPlugins.toast.showWithOptions).toHaveBeenCalled();
+  });
+
   it('logs message to the console if cordova is not available', () => {
-    ionicPlugins = new IonicPlugins(<any> new PlatformMockIsCore);
+    ionicPlugins = new IonicPlugins((<any> new PlatformMockIsCore), (<any> new ToastMock));
     spyOn(console, 'log');
     ionicPlugins.showToast(TestData.response.message);
     expect(console.log).toHaveBeenCalledWith(TestData.response.message);
