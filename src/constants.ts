a1a54aa9b3459d2309b770926f60c08b24f0bb44
Emmanuel Roussel <emman.roussel@gmail.com>
2017-04-19 23:40:05 -0400

    Remove exclamation points from messages

--- src/constants.ts
+++ src/constants.ts
@@ -20,15 +20,15 @@ export class Links {
 export class Messages {
   private static baseSingular = 'Item successfully ';
   private static basePlural = 'Item(s) successfully ';
-  public static itemAdded = `${Messages.baseSingular} added!`;
-  public static itemEdited = `${Messages.baseSingular} edited!`;
-  public static itemDeleted = `${Messages.baseSingular} deleted!`;
-  public static itemsReturned = `${Messages.basePlural} returned!`;
-  public static itemsRented = `${Messages.basePlural} rented!`;
-  public static itemAlreadyRented = 'Item is already rented!';
-  public static itemNotRented = 'Item is not currently rented!';
-  public static itemAlreadyAdded = 'Item is already added to the list!';
-  public static userEdited = 'User successfully edited!';
+  public static itemAdded = `${Messages.baseSingular} added`;
+  public static itemEdited = `${Messages.baseSingular} edited`;
+  public static itemDeleted = `${Messages.baseSingular} deleted`;
+  public static itemsReturned = `${Messages.basePlural} returned`;
+  public static itemsRented = `${Messages.basePlural} rented`;
+  public static itemAlreadyRented = 'Item is already rented';
+  public static itemNotRented = 'Item is not currently rented';
+  public static itemAlreadyAdded = 'Item is already added to the list';
+  public static userEdited = 'User successfully edited';
 }
 
 export class ItemProperties {
