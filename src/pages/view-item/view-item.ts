4a4bc274b9c9102473bdfba3dfda4421e71ee44f
Emmanuel Roussel <emman.roussel@gmail.com>
2017-05-01 16:10:12 -0400

    Update item in view-item when edited
    Subscribe to the event to update the item shown

--- src/pages/view-item/view-item.ts
+++ src/pages/view-item/view-item.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, Platform } from 'ionic-angular';
+import { NavController, NavParams, Platform, Events } from 'ionic-angular';
 
 import { Actions } from '../../constants';
+import { InventoryData } from '../../providers/inventory-data';
+import { Notifications } from '../../providers/notifications';
 import { EditItemPage } from '../edit-item/edit-item';
 
 @Component({
@@ -14,11 +16,21 @@ export class ViewItemPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public platform: Platform
+    public platform: Platform,
+    public events: Events,
+    public inventoryData: InventoryData,
+    public notifications: Notifications
   ) { }
 
   ngOnInit() {
     this.item = this.navParams.get('item');
+
+    this.events.subscribe('item:edited', barcode => {
+      this.inventoryData.getItem(barcode).subscribe(
+        item => this.item = item,
+        err => this.notifications.showToast(err)
+      );
+    });
   }
 
   editItem() {
