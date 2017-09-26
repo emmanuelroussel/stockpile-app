998a1e914d11db2d40a64cec54b000def8077adb
Emmanuel Roussel <emman.roussel@gmail.com>
2017-09-26 11:41:17 -0400

    Display brand along with model in list

--- src/pages/fields/fields.ts
+++ src/pages/fields/fields.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-import { Actions } from '../../constants';
+import { Actions, ItemProperties } from '../../constants';
 import { EditFieldPage } from '../edit-field/edit-field';
 
 import { MapToIterablePipe } from '../../pipes/map-to-iterable.pipe';
@@ -15,6 +15,7 @@ export class FieldsPage {
   fields: Observable<any>;
   type: string;
   typePlural: string;
+  itemProperties = ItemProperties;
 
   constructor(
     public navCtrl: NavController,
